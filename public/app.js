0c4e43822d620be09adee5f39e22aae6ead1e42b
DanielPurdy509 <danielpurdy509@gmail.com>
2023-04-12 13:59:00 -0700

    JS complete for togglign visibility on input forms

--- public/app.js
+++ public/app.js
@@ -1,30 +1,47 @@
-const app = express();
-app.use(express.static("public"));
+// const app = express();
+// app.use(express.static("public"));
 
-let container = document.querySelector("#card_front");
+// let container = document.querySelector("#card_front");
 
-fetch("/api/test")
-.then(function (response) {
-    return response.json();
+// fetch("/api/test")
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (users) {
+//     console.log(users);
+//     users.forEach(function (users) {
+//         console.log("Adding h2 for person:", users);
+//         container.innerHTML += `<h2>${users.name}</h2>`;
+//     });
+// });
+
+// Variables and Event Listeners for From Visiblity --------------------------------
+const addCardButton = document.querySelector("#add_new_button");
+const newCardForm = document.querySelector("#new_card_form");
+const loginForm = document.querySelector("#login_form");
+const login_button = document.querySelector("#login_button");
+const signup_button = document.querySelector("#signup_button");
+
+addCardButton.addEventListener("click",function() {
+    if(newCardForm.style.visibility === "visible") {
+        newCardForm.style.visibility = "hidden";
+      }else {
+        newCardForm.style.visibility = "visible";
+    }
 })
-.then(function (users) {
-    console.log(users);
-    users.forEach(function (users) {
-        console.log("Adding h2 for person:", users);
-        container.innerHTML += `<h2>${users.name}</h2>`;
-    });
-});
 
-// const addCardButton = document.querySelector("#add_new_button");
-// const newCardForm = document.querySelector("#new_card_form");
+login_button.addEventListener("click",function() {
+    if(loginForm.style.visibility === "visible") {
+        loginForm.style.visibility = "hidden";
+      }else {
+        loginForm.style.visibility = "visible";
+    }
+})
 
-// addCardButton.addEventListener("click",function() {
-//     newCardForm.classList.toggle("visible");
-// })
-
-// const loginForm = document.querySelector("#login_form")
-// const login_button = document.querySelector("#login_button")
-
-// login_button.addEventListener("click",function () {
-//     login_button.classList.toggle("visible");
-// })
+signup_button.addEventListener("click",function() {
+    if(loginForm.style.visibility === "visible") {
+        loginForm.style.visibility = "hidden";
+      }else {
+        loginForm.style.visibility = "visible";
+    }
+})
