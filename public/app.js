b0686253a57932eb522159ec549560e6d3fb8a0a
DanielPurdy509 <danielpurdy509@gmail.com>
2023-04-12 06:32:30 -0700

    All forms and buttons complete. Working on server-side, database, and client-side logic

--- public/app.js
+++ public/app.js
@@ -1,6 +1,13 @@
-let addCardButton = document.querySelector("add_card_button");
-let newCardForm = document.querySelector("new_card_form");
+const addCardButton = document.querySelector("#add_new_button");
+const newCardForm = document.querySelector("#new_card_form");
 
-addCardButton.addEventListener("click", () => {
-    newCardForm.style.visibility = "visible";
+addCardButton.addEventListener("click",function() {
+    newCardForm.classList.toggle("visible");
+})
+
+const loginForm = document.querySelector("#login_form")
+const login_button = document.querySelector("#login_button")
+
+login_button.addEventListener("click",function () {
+    login_button.classList.toggle("visible");
 })
